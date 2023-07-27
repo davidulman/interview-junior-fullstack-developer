71fa0f2e530aa8958ae57c21decdc5fd608bda01
David Ulman <david@ulmand.com>
2023-07-27 14:59:25 +0300

    BackEnd Complete

--- interview-backend/src/app.controller.ts
+++ interview-backend/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/city')
+  getCity(
+    @Body('cityName') cityName: string,
+    @Body('currentPage') currentPage: number,
+  ) {
+    return this.appService.getCity(cityName, currentPage);
   }
 }
--- interview-backend/src/app.service.ts
+++ interview-backend/src/app.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private cities = JSON.parse(readFileSync('../cities.json', 'utf8'));
+
+  getCity(cityName, currentPage) {
+    const maxResultsPerPage = 5;
+    const city = cityName.toLowerCase();
+    const filteredCities = this.cities.filter((c) =>
+      c.cityName.toLowerCase().includes(city),
+    );
+
+    const start = currentPage - 1;
+    const end = start + maxResultsPerPage;
+    const maxPages = Math.ceil(filteredCities.length / maxResultsPerPage);
+    const totalResultsInThisPage = filteredCities.slice(start, end).length;
+    const totalResults = filteredCities.length;
+
+    if (filteredCities.length === 0) {
+      throw new HttpException(
+        `No results found for ${cityName}`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    if (currentPage > maxPages) {
+      throw new HttpException(
+        `Page ${currentPage} doesn't excites there are only ${maxPages} pages `,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return {
+      cities: filteredCities.slice(start, end),
+      page: currentPage + '/' + maxPages,
+      totalResultsInThisPage,
+      totalResults,
+    };
   }
 }
